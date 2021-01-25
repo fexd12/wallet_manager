ac016e9404f6edc059291baa2efa95ac5a21732c
fexd12 <felipe.demelo@hotmail.com>
2021-01-24 23:21:23 -0300

    editado rota de ativos

--- backend/app/api/ativos/routes.py
+++ backend/app/api/ativos/routes.py
@@ -1,7 +1,8 @@
 from . import bp
 from app import cross_origin,db
 from flask import jsonify,request
-from app.models import Ativo,TipoAtivo
+from app.models import Ativo,Movimentacao,TipoAtivo
+from sqlalchemy import text
 
 @bp.route('/',methods=['GET','POST'])
 @cross_origin()
@@ -9,7 +10,6 @@ def ativos():
     
     if request.method == 'POST':
         data = request.get_json()
-        
         
         try:
             if request.content_length == 0: raise Exception("Favor inserir nome do ativo")
@@ -30,11 +30,38 @@ def ativos():
             }),500
 
     try:
+
+        with db.engine.connect() as conn:
+            query = text("""
+                SELECT  DISTINCT(mov.ativo_id),
+                        mov.id_movimentacao, 
+                        mov.carteira_id,
+                        atv.ticker,
+                        nome,
+                        atv.tipo_ativo_id,
+                        f_preco_medio(mov.ativo_id) as preco_medio
+                FROM movimentacao mov
+                join ativo atv on
+                    mov.ativo_id = atv.id_ativo
+                where mov.tipo ='C';
+            """)
+            result = conn.execute(query).fetchall()
         
-        ativos  = Ativo.query.all()
+        items = []
+
+        for row in list(result):
+            items.append({
+                "ativo_id":row[0],
+                "id_movimentacao":row[1],
+                "carteira_id":row[2],
+                "ticker":row[3],
+                "nome":row[4],
+                "tipo_ativo":row[5],
+                "preco_medio":float(row[6])
+            })
 
         return jsonify({
-            'data':[item.to_dict() for item in ativos]
+            'items':items
         }),200
     
     except Exception as e:
