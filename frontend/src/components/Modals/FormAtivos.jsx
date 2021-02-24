f9e82d15a2549616c04f3467a3ead55d829f8db5
fmelo <felipe.demelo@hotmail.com>
2021-02-23 23:56:34 -0300

    adicionando novos campos ao form de ativos e corrigindo post ao backend

--- frontend/src/components/Modals/FormAtivos.jsx
+++ frontend/src/components/Modals/FormAtivos.jsx
@@ -3,29 +3,40 @@ import Modal from 'react-bootstrap/Modal';
 import PropTypes from "prop-types";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form'
+import api from '../../sevices/api'
 
-const FormAtivos = ({tipos,showModal,onHide,handleSave,ativo}) => {
+const FormAtivos = ({showModal,onHide,handleSave,ativo}) => {
 
     const [ativosSubmit,setAtivosSubmit] = useState({
         ticker:'',
-        tipo:'C',
-        tipoAtivo:1,
-        preco:0,
+        tipo:'c',
+        tipoAtivo:0,
+        preco:0.00,  
         quantidade:0,
-        taxas:0
+        taxas:0.00,
+        data:''
 
     })
 
     const handleChange = (ev)=>{
         ev.preventDefault();
         ev.target.name === 'ticker' ? setAtivosSubmit({...ativosSubmit,ticker:ev.target.value}) : 
-        ev.target.name === 'tipoAtivo'  ? setAtivosSubmit({...ativosSubmit,tipo:ev.target.value}) : 
+        ev.target.name === 'tipo' ? setAtivosSubmit({...ativosSubmit,tipo:ev.target.value}) :
         ev.target.name === 'preco' ? setAtivosSubmit({...ativosSubmit,preco:ev.target.value}) :
         ev.target.name === 'quantidade' ? setAtivosSubmit({...ativosSubmit,quantidade:ev.target.value}) : 
         ev.target.name === 'taxas' ? setAtivosSubmit({...ativosSubmit,taxas:ev.target.value}) : 
+        ev.target.name === 'data' ? setAtivosSubmit({...ativosSubmit,data:ev.target.value}) : 
         setAtivosSubmit({...ativosSubmit})
     }
 
+    const handleSubmit = (ev) =>{
+
+        ev.preventDefault();
+        
+        setAtivosSubmit({...ativosSubmit,tipoAtivo:ativo.id_tipo_ativo});
+        console.log(ativo.id_tipo_ativo)
+        handleSave('/movimentacao/',ativosSubmit);
+    }
 
     return (
         <Modal show = {showModal} onHide={onHide}>
@@ -34,23 +45,23 @@ const FormAtivos = ({tipos,showModal,onHide,handleSave,ativo}) => {
             </Modal.Header>
             
             <Modal.Body>
-                <Form method='post' onSubmit={() => handleSave('/movimentacao/',ativosSubmit)}>
+                <Form onSubmit={handleSubmit}>
 
                     <Form.Group controlId='formAtivo' onChange={handleChange}>
                         <Form.Label>Ticker Ativo</Form.Label>
                         <Form.Control type='' name='ticker'></Form.Control>
                     </Form.Group>
 
-                    <Form.Group controlId='formTipo'  onChange={handleChange}>
-                        <Form.Label>Tipo do Ativo</Form.Label>
-                        <Form.Control as='select' name='tipoAtivo'>
-                        {tipos.length > 0 ? tipos.map(x=><option key={x.id_tipo_ativo} value={x.id_tipo_ativo}>{x.nome} </option>) : null}
+                    <Form.Group controlId='formAtivo'  onChange={handleChange}>
+                        <Form.Label>Tipo</Form.Label>
+                        <Form.Control as='select' name='tipo'>
+                        {['Compra','Venda'].map((x,index)=><option key={index} value={x === 'Compra' ? 'c' : 'v'}>{x} </option>)}
                         </Form.Control>
                     </Form.Group>
 
                     <Form.Group controlId='formAtivo' onChange={handleChange}>
                         <Form.Label>Preço</Form.Label>
-                        <Form.Control type='number' name='preco'></Form.Control>
+                        <Form.Control name='preco'></Form.Control>
                     </Form.Group>
 
                     <Form.Group controlId='formAtivo'  onChange={handleChange}>
@@ -62,7 +73,12 @@ const FormAtivos = ({tipos,showModal,onHide,handleSave,ativo}) => {
                         <Form.Label>Taxas</Form.Label>
                         <Form.Control type='number' placeholder='Informe as taxas,se houver' name='taxas'></Form.Control>
                     </Form.Group>
-                    
+
+                    <Form.Group controlId='formAtivo'  onChange={handleChange}>
+                        <Form.Label>Data</Form.Label>
+                        <Form.Control type='date' placeholder='Informe a Data' name='data'></Form.Control>
+                    </Form.Group>
+
                     <Button type='submit'>Adicionar Ativo</Button>
 
                 </Form>
@@ -81,8 +97,8 @@ FormAtivos.propTypes ={
     showModal: PropTypes.bool,
     onHide: PropTypes.func,
     handleSave: PropTypes.func,
-    ativo: PropTypes.object,
-    tipos:PropTypes.array
+    ativo: PropTypes.object
+    
 }
 
 export default FormAtivos;
